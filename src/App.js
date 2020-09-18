b73b801b849c9238560d1e776bd802f2df05290b
Travis Bowen <Travisbowen23@gmail.com>
2020-09-17 20:37:00 -0700

    Finished login component style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
 	return (
 		<div className='app'>
-			<h1>App</h1>
+			<Login />
 		</div>
 	);
 }
---
+++ src/utils/spotify.js
@@ -0,0 +1,3 @@
+// Endpoint for Spotify's auth
+export const authEndpoint = "https://account.spotify.com/authorize";
+
