c3f5bdb1ae863003bfae698d019d9ed8c2cfaa6c
Travis Bowen <Travisbowen23@gmail.com>
2020-09-26 14:05:01 -0700

    Fixed bug with sidebar titles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,9 +16,9 @@ function Sidebar() {
 				src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg'
 				alt='spotify logo'
 			/>
-			<SidebarOption text='Home' Icon={HomeIcon} />
-			<SidebarOption text='Search' Icon={SearchIcon} />
-			<SidebarOption text='Your Library' Icon={LibraryMusicIcon} />
+			<SidebarOption title='Home' Icon={HomeIcon} />
+			<SidebarOption title='Search' Icon={SearchIcon} />
+			<SidebarOption title='Your Library' Icon={LibraryMusicIcon} />
 
 			<br />
 			<strong className='sidebar__title'>PLAYLISTS</strong>
